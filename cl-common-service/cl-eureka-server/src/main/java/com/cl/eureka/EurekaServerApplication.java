376b87966d85511338d937443b68b38cd955a9c2
chenglong <chenglong@weilaisihang.com>
2018-06-20 21:02:44 +0800

    通用服务结构提交

--- cl-common-service/cl-eureka-server/src/main/java/com/cl/eureka/EurekaServerApplication.java
+++ cl-common-service/cl-eureka-server/src/main/java/com/cl/eureka/EurekaServerApplication.java
@@ -1,8 +1,19 @@
 package com.cl.eureka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @author:chenglong
  * @dateTime:2018/6/19 18:50
  */
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
 }
--- cl-common-service/cl-zuul-server/src/main/java/com/cl/zuul/ZuulServerApplication.java
+++ cl-common-service/cl-zuul-server/src/main/java/com/cl/zuul/ZuulServerApplication.java
@@ -1,8 +1,20 @@
 package com.cl.zuul;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * @author:chenglong
  * @dateTime:2018/6/19 18:51
  */
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableZuulProxy
 public class ZuulServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+
 }
