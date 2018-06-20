5255d5e163db701781805f17f4b4bcdcb8bf66de
chenglong <chenglong@weilaisihang.com>
2018-06-20 18:34:12 +0800

    config server 启动 springcloud Finchley 版本在mvn中央仓库没有需要添加spring仓库

--- cl-common-service/cl-config-server/src/main/java/com/cl/config/ConfigServerApplication.java
+++ cl-common-service/cl-config-server/src/main/java/com/cl/config/ConfigServerApplication.java
@@ -1,8 +1,20 @@
 package com.cl.config;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @author:chenglong
  * @dateTime:2018/6/19 18:50
  */
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
